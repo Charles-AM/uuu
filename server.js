b209732a9e4906b784c5b2de74f10f794440072e
Charles-AM <108084320+Charles-AM@users.noreply.github.com>
2023-12-18 09:14:43 +0000

    Update server.js

--- server.js
+++ server.js
@@ -49,3 +49,7 @@ app.get('/viewPatient/:patientID', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
